53d4d3b2736a4d2be891e6fa8832847608187e2d
Girl -Alzahra <Fatmah@JZG-Fatmah.JZGAT02>
2026-05-29 19:35:22 +0300

    Merged server and frontend into a single production repo

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: "*", 
+        methods: ["GET", "POST"]
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Signaling Server is Running Securely Online!');
+});
+
+io.on('connection', (socket) => {
+    console.log(`⚡ جهاز متصل أون لاين: ${socket.id}`);
+    socket.on('join-studio', (data) => socket.broadcast.emit('join-studio', data));
+    socket.on('webrtc-offer', (data) => socket.broadcast.emit('webrtc-offer', data));
+    socket.on('webrtc-answer', (data) => socket.broadcast.emit('webrtc-answer', data));
+    socket.on('webrtc-candidate', (data) => socket.broadcast.emit('webrtc-candidate', data));
+    socket.on('disconnect', () => io.emit('device-disconnected', socket.id));
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
